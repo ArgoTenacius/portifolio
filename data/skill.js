010ce2ac2ff841374f3bfd5dfde0382f7c395605
Ramon Mauricio <ArgoTenacius@gmail.com>
2022-05-24 20:10:06 -0300

    Setup.

---
+++ data/skill.js
@@ -0,0 +1,38 @@
+export const skill = [
+    {
+        name: "CSS",
+        photo: "https://img.icons8.com/color/64/css3.png"
+    },
+    {
+        name: "HTML",
+        photo: "https://img.icons8.com/color/64/html-5--v1.png"
+    },
+    {
+        name: "JavaScript",
+        photo: "https://img.icons8.com/color/64/javascript--v1.png"
+    },
+    {
+        name: "Github",
+        photo: "https://img.icons8.com/ios-glyphs/64/github.png"
+    },
+    {
+        name: "Git",
+        photo: "https://img.icons8.com/color/64/git.png"
+    },
+    {
+        name: "React",
+        photo: "https://img.icons8.com/cute-clipart/64/react-native.png"
+    },
+    {
+        name: "Firebase",
+        photo: "https://img.icons8.com/color/64/firebase.png"
+    },
+    {
+        name: "Npm",
+        photo: "https://img.icons8.com/color/64/npm.png"
+    },
+    {
+        name: "Webpack",
+        photo: "https://img.icons8.com/color/64/webpack.png"
+    }
+]
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './app.css'
+
+const App = () => {
+  return (
+    <main>
+        App
+    </main>
+  )
+}
+
+export default App
