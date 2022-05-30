636f6e2539d9a670f642478df3b682f97cd100e4
Ramon Mauricio <ArgoTenacius@gmail.com>
2022-05-30 20:41:13 -0300

    Font size changed.

--- src/components/aboutMe/AboutMe.jsx
+++ src/components/aboutMe/AboutMe.jsx
@@ -5,7 +5,7 @@ const AboutMe = () => {
   return (
       <section className='about'>
           <p className='about__text'>
-              I'm a self-taught front-end programmer, i started to learn programming as a hobby and then decided to take it more
+              I'm a self-taught front-end programmer, started to learn programming as a hobby and then decided to take it more
               serious and started to do <a href='https://www.theodinproject.com/' target='_blank' rel='noreferrer'>The Odin Project</a>,
               now i look to learn even more with the professionals.
           </p>
