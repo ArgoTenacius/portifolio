df18c9dae59e1269ffb1e57a2b8c94db5b5e4fe7
Ramon Mauricio <ArgoTenacius@gmail.com>
2022-07-17 12:39:19 -0300

    Updated list

--- src/data/project.js
+++ src/data/project.js
@@ -7,17 +7,17 @@ export const project = [
         description: "For study purpose, the main page of the Taco bell website (https://www.tacobell.co.in/) was recreated with the objective of optimize it the max possible, getting a better performance of 90%."
     },
     {
+        name: "Chat app",
+        repository: "https://github.com/ArgoTenacius/chat-app",
+        photo: "https://user-images.githubusercontent.com/94071579/178111155-b70d096c-4b46-4a0b-b500-d98577f1b49e.png",
+        live: "https://argotenacius.github.io/chat-app/",
+        description: "A real-time web chat application inspired by WhatsApp Web. Using firebase authentication and database, it can send and receive messages dynamically."
+    },
+    {
         name: "Bug tracker",
         repository: "https://github.com/ArgoTenacius/bug-tracker",
         photo: "https://user-images.githubusercontent.com/94071579/170094494-c4edc3b3-6f34-43c0-8764-53ccd79b5919.png",
         live: "https://argotenacius.github.io/bug-tracker/",
         description: "With firebase in mind as the database, the project was made to learn how to apply, get, delete, move and add database info, getting in result an authentication system and a dynamic data management."
     },
-    {
-        name: "Where's Waldo ?",
-        repository: "https://github.com/ArgoTenacius/wheres-waldo",
-        photo: "https://user-images.githubusercontent.com/94071579/167257218-cd9b9844-f8e8-472a-ae10-8733a15bcc1e.png",
-        live: "https://argotenacius.github.io/wheres-waldo/",
-        description: "A game project with the purpose to introduce the firebase, a platform developed by Google for creating mobile and web applications."
-    }
 ]
